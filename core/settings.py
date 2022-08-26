233b8c090060900ab5a0ead285131c4885ed56b5
Auracule <daabzpinky@gmail.com>
2022-08-26 08:55:19 +0100

    another core

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&91ax!6x2g7c!cw-_3dc#by3svsfaks7fly7tog8ag-ahny+18
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.81.230.135','*']
+ALLOWED_HOSTS = ['184.72.211.26','*']
 
 
 # Application definition
